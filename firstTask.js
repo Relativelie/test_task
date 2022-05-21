b0883f76a95fbbe5b8b1cf68bcc0cb66bccdf970
Relativelie <sopio1992@gmail.com>
2022-05-21 19:45:04 +0300

    add first task, eslint

---
+++ firstTask.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const concat = require('concat');
+const read = require('fs-readdir-recursive');
+
+const rootPath = path.join('../', 'textFiles');
+
+const foundFiles = read(rootPath).filter((item) => item.endsWith('.txt'));
+foundFiles.sort((a, b) => path.parse(a).name.localeCompare(path.parse(b).name));
+
+for (let i = 0; i < foundFiles.length; i++) {
+    const absolutePath = `../textFiles/${foundFiles[i]}`;
+    foundFiles[i] = absolutePath;
+}
+concat(foundFiles, 'resultOfConcat.txt');
