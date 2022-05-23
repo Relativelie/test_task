307ac4b09953763f7b3bd601209fef88426fb96e
Relativelie <sopio1992@gmail.com>
2022-05-23 09:10:56 +0300

    code refactoring

--- scripts/firstTask/firstTask.js
+++ scripts/firstTask/firstTask.js
@@ -5,9 +5,4 @@ const read = require('fs-readdir-recursive');
 const rootPath = path.join(__dirname);
 const foundFiles = read(rootPath).filter((item) => item.endsWith('.txt'));
 foundFiles.sort((a, b) => path.parse(a).name.localeCompare(path.parse(b).name));
-
-for (let i = 0; i < foundFiles.length; i++) {
-    const absolutePath = `../../../textFiles/${foundFiles[i]}`;
-    foundFiles[i] = absolutePath;
-}
 concat(foundFiles, 'resultOfConcat.txt');
--- scripts/secondTask/server/db.js
+++ scripts/secondTask/server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'home',
-    password: 'root',
+    user: 'your_user_name',
+    password: 'your_pass',
     host: 'localhost',
     port: 5432,
-    database: 'node_postgres',
+    database: 'your_database',
 });
 
 module.exports = pool;
