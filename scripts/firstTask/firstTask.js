b5cf429327e2dc7e125a4bd5521ae31404848775
Relativelie <sopio1992@gmail.com>
2022-05-24 21:44:26 +0300

    commit

--- scripts/firstTask/firstTask.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const concat = require('concat');
-const read = require('fs-readdir-recursive');
-
-const rootPath = path.join(__dirname);
-const foundFiles = read(rootPath).filter((item) => item.endsWith('.txt'));
-foundFiles.sort((a, b) => path.parse(a).name.localeCompare(path.parse(b).name));
-concat(foundFiles, 'resultOfConcat.txt');
--- scripts/secondTask/server/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    surname VARCHAR(255),
-    midname VARCHAR(255),
-    birthday DATE,
-    trainingGroup SERIAL
-);
