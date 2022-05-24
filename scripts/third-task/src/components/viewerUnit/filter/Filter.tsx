40f82cd6ba6e0c51d1e87ec5a3e93f2fd27a35e6
Relativelie <sopio1992@gmail.com>
2022-05-24 14:11:29 +0300

    add style for viewer block

--- scripts/third-task/src/components/viewerUnit/filter/Filter.tsx
+++ scripts/third-task/src/components/viewerUnit/filter/Filter.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import filterImg from '../../../assets/images/filter.png';
+import './Filter.scss';
 
 interface Props {
     changeFilterState: Function;
@@ -6,8 +8,14 @@ interface Props {
 
 export const Filter: FC<Props> = ({ changeFilterState }) => {
     return (
-        <div>
+        <div className="viewerUnit__filterBlock">
+            <img
+                className="viewerUnit__filterBlock__filterImage"
+                src={filterImg}
+                alt="filterLogo"
+            />
             <button
+                className="viewerUnit__filterBlock__filterBtn"
                 onClick={() => changeFilterState()}
                 type="button"
             >
--- scripts/third-task/src/components/viewerUnit/tasks/Tasks.tsx
+++ scripts/third-task/src/components/viewerUnit/tasks/Tasks.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { DataType } from '../../../types/dataType';
 
+import './Tasks.scss';
+
 interface Props {
     data: DataType[],
     prepareDateValue: Function
@@ -8,7 +10,7 @@ interface Props {
 
 export const Tasks:FC<Props> = ({ data, prepareDateValue }) => {
     return (
-        <div>
+        <div className="viewerUnit__tasks">
             {data.map((item) => {
                 const {
                     id, name, status, date, shortDesc,
@@ -20,15 +22,18 @@ export const Tasks:FC<Props> = ({ data, prepareDateValue }) => {
                 const month = `${prepareDateValue(fullDate.getMonth())}${fullDate.getMonth()}`;
                 const convertedDate = `${dateNum}.${month}.${fullDate.getFullYear()} ${hours}:${minutes}`;
                 return (
-                    <div key={`task-${id}`}>
-                        <p>{name}</p>
-                        <p>{shortDesc}</p>
-                        <p>
+                    <div
+                        className="viewerUnit__task"
+                        key={`task-${id}`}
+                    >
+                        <h3 className="viewerUnit__task__name">{name}</h3>
+                        <p className="viewerUnit__task__description">{shortDesc}</p>
+                        <p className="viewerUnit__task__status">
                             Статус задачи:
                             {' '}
                             {status ? 'close' : 'open'}
                         </p>
-                        <p>{convertedDate}</p>
+                        <p className="viewerUnit__task__date">{convertedDate}</p>
                     </div>
                 );
             })}
